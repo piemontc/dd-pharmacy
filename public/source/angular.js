0a99cc696bb1871eecd5bc2c43a58fdb23332b28
cpie19 <piemontechris@gmail.com>
2018-06-14 22:01:08 -0400

    Material re-design

--- public/source/angular.js
+++ public/source/angular.js
@@ -1,1 +1,22 @@
 var app = angular.module('app', ['ngRoute', 'ngMaterial', 'ngMessages']);
+
+app.config(function ($routeProvider) {
+
+    $routeProvider
+
+        .when('/', {
+            templateUrl: 'pages/home.html',
+            controller: 'mainController'
+        })
+
+        .when('/services', {
+            templateUrl: 'pages/services.html',
+            controller: 'mainController'
+        })
+
+        .when('/contact', {
+            templateUrl: 'pages/contact.html',
+            controller: 'mainController'
+        })
+
+});
